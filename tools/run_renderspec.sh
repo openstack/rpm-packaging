9bbc5884720a416951903a12778426fa3552e6cb
Haikel Guemar <hguemar@fedoraproject.org>
2016-07-17 20:06:14 +0200

    tools: parallelize execution
    Parallelize renderspec and spec-cleaner executions to speed up scripts.
    There is no constraint to run these jobs sequentially
    
    Change-Id: I7bdeee82cc360cf31e9684ef22638f41c53b12b8

--- tools/run_renderspec.sh
+++ tools/run_renderspec.sh
@@ -8,6 +8,7 @@ specdir=${basedir}/openstack/
 WORKSPACE=${WORKSPACE:-$basedir}
 OUTPUTDIR=$WORKSPACE/logs/
 specstyles="suse fedora"
+MAXPROC=4
 
 mkdir -p $OUTPUTDIR
 
@@ -16,11 +17,16 @@ for specstyle in $specstyles; do
     rm -f $OUTPUTDIR/*.${specstyle}
 done
 
+count=0
 echo "run renderspec over specfiles from ${specdir}"
 for spec in ${specdir}/**/*.spec.j2; do
     for specstyle in $specstyles; do
         echo "run ${spec} for ${specstyle}"
         renderspec --spec-style ${specstyle} ${spec} \
-                   -o $WORKSPACE/logs/${spec##*/}.${specstyle}
+                   -o $WORKSPACE/logs/${spec##*/}.${specstyle} &
+        let count+=1
+        [[ count -eq $MAXPROC ]] && wait && count=0
     done
 done
+
+wait
